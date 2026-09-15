ce1a61e1acbb816d451645f677001817bac4ddda
GenoL02 <duongthanhlong02@gmail.com>
2026-09-15 16:58:25 +0700

    test deploy github 555

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/mcp-app",
   server: {
     proxy: {
       "/mcp": {
@@ -12,5 +13,4 @@ export default defineConfig({
       },
     },
   },
-  base: "/mcp-app",
 });
